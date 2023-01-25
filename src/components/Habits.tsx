4486edbc93e3a2a364b445b9d818df26addf9c83
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-01-25 15:04:57 -0300

    chore: Habits: refinements on component

--- src/components/Habits.tsx
+++ src/components/Habits.tsx
@@ -1,9 +1,11 @@
-import "./Habit.css";
-
 interface IHabit {
   completed: number;
 }
 
 export function Habit({ ...props }: IHabit) {
-  return <div className="habit">{props.completed}</div>;
+  return (
+    <div className="bg-zinc-900 w-10 h-10 text-white rounded m-2 flex text-center items-center justify-center">
+      {props.completed}
+    </div>
+  );
 }
