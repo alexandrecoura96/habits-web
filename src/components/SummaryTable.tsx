136469cfc56c306b81c909829ccb38b00fdcc5dd
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-02-04 14:31:25 -0300

    chore: SummaryTable: component created

---
+++ src/components/SummaryTable.tsx
@@ -0,0 +1,29 @@
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
+import { HabitDay } from "./HabitDay";
+
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+
+const summaryDates = generateDatesFromYearBeginning();
+
+export function SummaryTable() {
+  return (
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
+        {weekDays.map((item, index) => (
+          <div
+            key={`${item} + ${index}`}
+            className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        {summaryDates.map((date) => {
+          return <HabitDay key={date.toString()} />;
+        })}
+      </div>
+    </div>
+  );
+}
