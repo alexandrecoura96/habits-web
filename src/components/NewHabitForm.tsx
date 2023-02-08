a09275479bc82ec4b55713847b0b434d01cc9f39
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-02-07 23:27:48 -0300

    chore: NewHabitForm: post method implemented

--- src/components/NewHabitForm.tsx
+++ src/components/NewHabitForm.tsx
@@ -1,6 +1,7 @@
 import { Check } from "phosphor-react";
 import * as Checkbox from "@radix-ui/react-checkbox";
 import { FormEvent, useState } from "react";
+import { api } from "../lib/axios";
 
 const avaliableWeekDays = [
   "Domingo",
@@ -16,8 +17,22 @@ export function NewHabitForm() {
   const [title, setTitle] = useState("");
   const [weekDays, setWeekDays] = useState<number[]>([]);
 
-  function createNewHabit(event: FormEvent) {
+  async function createNewHabit(event: FormEvent) {
     event.preventDefault();
+
+    if (!title || weekDays.length === 0) {
+      return;
+    }
+
+    await api.post("habits", {
+      title,
+      weekDays,
+    });
+
+    setTitle("");
+    setWeekDays([]);
+
+    alert("Hábito criado com sucesso");
   }
 
   function handleToogleWeekDay(weekDay: number) {
@@ -41,7 +56,8 @@ export function NewHabitForm() {
         placeholder="ex.: Exercícios, dormir bem, etc.."
         autoFocus
         className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
-        onChange={(event) => event.target.value}
+        onChange={(event) => setTitle(event.target.value)}
+        value={title}
       />
       <label htmlFor="" className="font-semibold leading-tight mt-4">
         Qual a recorrência?
@@ -51,6 +67,7 @@ export function NewHabitForm() {
           <Checkbox.Root
             key={`${weekday} + ${index}`}
             className="flex items-center gap-3 group"
+            checked={weekDays.includes(index)}
             onCheckedChange={() => handleToogleWeekDay(index)}
           >
             <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
