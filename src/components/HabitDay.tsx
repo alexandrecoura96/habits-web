530fc6d51adf8cfe7a7ae851f6c6d19ac196ee3e
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-02-08 11:35:39 -0300

    chore: HabitDay: improvements

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -3,25 +3,36 @@ import clsx from "clsx";
 import { ProgressBar } from "./ProgressBar";
 import dayjs from "dayjs";
 import { HabitsList } from "./HabitsList";
+import { useState } from "react";
 
 interface IHabitDay {
   date: Date;
-  completed?: number;
+  defaultCompleted?: number;
   ammount?: number;
 }
 
-export function HabitDay({ date, ammount = 0, completed = 0 }: IHabitDay) {
+export function HabitDay({
+  date,
+  ammount = 0,
+  defaultCompleted = 0,
+}: IHabitDay) {
+  const [completed, setCompleted] = useState(defaultCompleted);
+
   const completedPercentage =
     ammount > 0 ? Math.round((completed / ammount) * 100) : 0;
 
   const dayAndMonth = dayjs(date).format("DD/MM");
   const dayOfWeek = dayjs(date).format("dddd");
 
+  function handleCompletedChange(completed: number) {
+    setCompleted(completed);
+  }
+
   return (
     <Popover.Root>
       <Popover.Trigger
         className={clsx(
-          "w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg",
+          "w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-background",
           {
             "bg-zinc-900 border-zinc-800": completedPercentage === 0,
             "bg-violet-900 border-violet-500":
@@ -37,13 +48,13 @@ export function HabitDay({ date, ammount = 0, completed = 0 }: IHabitDay) {
         )}
       />
       <Popover.Portal>
-        <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
+        <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-background">
           <span className="font-semibold text-zinc-400">{dayOfWeek}</span>
           <span className="mt-1 font-extrabold leading-tight text-3xl">
             {dayAndMonth}
           </span>
           <ProgressBar progress={completedPercentage} />
-          <HabitsList date={date} />
+          <HabitsList date={date} onCompletedChange={handleCompletedChange} />
           <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
         </Popover.Content>
       </Popover.Portal>
