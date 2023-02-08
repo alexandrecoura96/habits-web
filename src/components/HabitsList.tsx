30e2210c03180b44cd76e4c34223f4eebb77c575
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-02-08 11:35:34 -0300

    chore: HabitsList: improvements

--- src/components/HabitsList.tsx
+++ src/components/HabitsList.tsx
@@ -6,6 +6,7 @@ import { api } from "../lib/axios";
 
 interface IHabitsList {
   date: Date;
+  onCompletedChange: (completed: number) => void;
 }
 
 interface IHabitsInfo {
@@ -17,7 +18,7 @@ interface IHabitsInfo {
   completedHabits: string[];
 }
 
-export function HabitsList({ date }: IHabitsList) {
+export function HabitsList({ date, onCompletedChange }: IHabitsList) {
   const [habitsInfo, setHabitsInfo] = useState<IHabitsInfo>();
 
   useEffect(() => {
@@ -52,6 +53,7 @@ export function HabitsList({ date }: IHabitsList) {
       possibleHabits: habitsInfo!.possibleHabits,
       completedHabits,
     });
+    onCompletedChange(completedHabits.length);
   }
 
   const isDateInPast = dayjs(date).endOf("day").isBefore(new Date());
@@ -65,9 +67,9 @@ export function HabitsList({ date }: IHabitsList) {
             onCheckedChange={() => handleToogleHabit(habit.id)}
             checked={habitsInfo.completedHabits.includes(habit.id)}
             disabled={isDateInPast}
-            className="flex items-center gap-3 group"
+            className="flex items-center gap-3 group focus:outline-none disabled:cursor-not-allowed"
           >
-            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-700 group-focus:ring-offset-2 group-focus:ring-offset-background">
               <Checkbox.CheckboxIndicator>
                 <Check size={20} className="text-white" />
               </Checkbox.CheckboxIndicator>
